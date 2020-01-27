1e296a3619eefdcdaa07b4963ab970e6dd8c4de9
Maikel Mendez <maikel_m@yahoo.com>
2020-01-27 07:05:36 -0600

    Add files via upload

---
+++ R_Libraries_Maikel.R
@@ -0,0 +1,67 @@
+# //////////////////////////////////////////////////////////////////////////////////
+# INSTITUTO TECNOLOGICO DE COSTA RICA
+# Escuela de Ingenieria en Construccion
+# https://www.tec.ac.cr
+
+# Eng. MSc. Maikel Mendez Morales
+# email: maikel.mendez@gmail.com mamendez@itcr.ac.cr
+# LinkedIn: https://www.linkedin.com/pub/maikel-mendez-morales/2a/738/1b9
+# Google Scholar: https://scholar.google.com/citations?hl=en&user=JnmSVFYAAAAJ
+# YouTube: https://www.youtube.com/channel/UCgpHYoMoZ059e508WVWiMhw
+# //////////////////////////////////////////////////////////////////////////////////
+
+# Workspace is cleared
+rm(list = ls())
+
+# Please install the following R-libraries in your Windows System:
+install.packages("Agreement")
+install.packages("arm")
+install.packages("bbmle")
+install.packages("binhf")
+install.packages("car")
+install.packages("corrgram")
+install.packages("corrplot")
+install.packages("DescTools")
+install.packages("df.spss")
+install.packages("dplyr")
+install.packages("effects")
+install.packages("foreign")
+install.packages("GGally")
+install.packages("ggalt")
+install.packages("ggplot2")
+install.packages("ggthemes")
+install.packages("glmulti")
+install.packages("Hmisc")
+install.packages("lattice")
+install.packages("lubridate")
+install.packages("MASS")
+install.packages("mgcv")
+install.packages("mosaic")
+install.packages("mosaicData")
+install.packages("nls2")
+install.packages("nlstools")
+install.packages("pastecs")
+install.packages("plyr")
+install.packages("propagate")
+install.packages("proto")
+install.packages("reshape")
+install.packages("reshape2")
+install.packages("rJava")
+install.packages("sjPlot")
+install.packages("tidyr")
+install.packages("vcd")
+install.packages("vcdExtra")
+install.packages("visreg")
+install.packages("XLConnect")
+install.packages("xlsx")
+install.packages("JGR")
+install.packages("rJava")
+install.packages("JavaGD")
+install.packages("iplots")
+install.packages("Deducer")
+install.packages("DeducerExtras")
+install.packages("DeducerPlugInExample")
+install.packages("DeducerPlugInScaling")
+install.packages("DeducerSpatial")
+install.packages("DeducerText")
+install.packages("quantreg")
